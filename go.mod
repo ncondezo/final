7510fa30eb122b5f45c714f91f909466b01bf9b6
Nicolas Condezo <nicolascondezo@Nicolass-MBP.fibertel.com.ar>
2023-11-28 20:22:01 -0300

    clean

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ncondezo/final
-
-go 1.21.3
--- internal/dentists/query.go
+++
@@ -1,8 +0,0 @@
-package dentists
-
-var(
-	QueryInsertDentist = `INSERT INTO storage.products(name,last_name,registration) VALUES(?,?,?)`
-	QueryGetDentistById = `SELECT name, last_name, registration FROM storage.dentists WHERE id = ?`
-	QueryUpdateDentist = `UPDATE storage.dentists SET name = ?, last_name = ?, registration = ? WHERE id = ?`
-	QueryDeleteDentist  = `DELETE FROM storage.dentists WHERE id = ?`
-)
