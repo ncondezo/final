b928f577fa6142b3c140532f4200efebc0eaa977
rodribatista <rodribatista@icloud.com>
2023-11-29 20:54:17 -0300

    add dentist and patient sql init

--- pkg/store/init.sql
+++ pkg/store/init.sql
@@ -13,3 +13,29 @@ CREATE TABLE users
     CONSTRAINT users_email
         UNIQUE (email)
 );
+
+CREATE TABLE IF NOT EXISTS DENTISTS
+(
+    id       VARCHAR(100) NOT NULL,
+    name     VARCHAR(25)  NOT NULL,
+    surname  VARCHAR(25)  NOT NULL,
+    registry VARCHAR(10)  NOT NULL,
+    CONSTRAINT dentists_id
+        PRIMARY KEY (id),
+    CONSTRAINT dentists_registry
+        UNIQUE (registry)
+);
+
+CREATE TABLE IF NOT EXISTS PATIENTS
+(
+    id            VARCHAR(100) NOT NULL,
+    name          VARCHAR(25)  NOT NULL,
+    surname       VARCHAR(25)  NOT NULL,
+    address       VARCHAR(250) NOT NULL,
+    document      VARCHAR(10)  NOT NULL,
+    register_date DATE         NOT NULL,
+    CONSTRAINT patients_id
+        PRIMARY KEY (id),
+    CONSTRAINT patients_document
+        UNIQUE (document)
+);
